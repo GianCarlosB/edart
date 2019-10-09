4cf9a41647009d13df6da474e5e03590e37706b8
Gian Honório <giancarlosf12@gmail.com>
2019-10-09 19:59:06 -0300

    Aumentar o tamanho da bandeira do idioma selecionado

--- js/language.js
+++ js/language.js
@@ -4,11 +4,15 @@
 	$("#language-portuguese").click(function() {
 		$('.br').hide();
 		$('.pt').show();
+		$('#language-portuguese').addClass("active-flag");
+		$('#language-brazilian-portuguese').removeClass("active-flag");
 	});
 
 	$("#language-brazilian-portuguese").click(function(){
 		$('.pt').hide();
 		$('.br').show();
+		$('#language-brazilian-portuguese').addClass("active-flag");
+		$('#language-portuguese').removeClass("active-flag");
 	});
 	
 	$('#language-portuguese').click();
