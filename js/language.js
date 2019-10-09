4a7c30f33fbcbf8bafce290367f9600732334f5d
Gian Honório <giancarlosf12@gmail.com>
2019-10-09 20:09:59 -0300

    Adição das funções da bandeira do idioma inglês

--- js/language.js
+++ js/language.js
@@ -2,17 +2,30 @@
   "use strict";
 	
 	$("#language-portuguese").click(function() {
-		$('.br').hide();
 		$('.pt').show();
+		$('.br').hide();
+		$('.en').hide();
 		$('#language-portuguese').addClass("active-flag");
 		$('#language-brazilian-portuguese').removeClass("active-flag");
+		$('#language-english').removeClass("active-flag");
 	});
 
 	$("#language-brazilian-portuguese").click(function(){
 		$('.pt').hide();
 		$('.br').show();
-		$('#language-brazilian-portuguese').addClass("active-flag");
+		$('.en').hide();
 		$('#language-portuguese').removeClass("active-flag");
+		$('#language-brazilian-portuguese').addClass("active-flag");
+		$('#language-english').removeClass("active-flag");
+	});
+	
+	$("#language-english").click(function(){
+		$('.pt').hide();
+		$('.br').hide();
+		$('.en').show();
+		$('#language-portuguese').removeClass("active-flag");
+		$('#language-brazilian-portuguese').removeClass("active-flag");
+		$('#language-english').addClass("active-flag");
 	});
 	
 	$('#language-portuguese').click();
