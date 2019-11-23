aa896e4a3bc61ba810ae1c472e779654e4409eb6
Gian Honório <giancarlosf12@gmail.com>
2019-11-23 17:51:51 -0300

    Adição do idioma francês

--- js/language.js
+++ js/language.js
@@ -5,10 +5,12 @@
 		$('.br').show();
 		$('.pt').hide();
         $('.es').hide();
+		$('.fr').hide();
 		$('.en').hide();
 		$('#language-brazilian-portuguese').addClass('active-flag');
 		$('#language-portuguese').removeClass('active-flag');
         $('#language-spanish').removeClass('active-flag');
+		$('#language-french').removeClass('active-flag');
 		$('#language-english').removeClass('active-flag');
 		var currentSlide = $('#ensign-nivoslider').data('nivo:vars').currentSlide;
 		if(currentSlide === 0 && $('#language-brazilian-portuguese').hasClass('active-flag')) {
@@ -20,10 +22,12 @@
 		$('.br').hide();
 		$('.pt').show();
         $('.es').hide();
+		$('.fr').hide();
 		$('.en').hide();
 		$('#language-brazilian-portuguese').removeClass('active-flag');
 		$('#language-portuguese').addClass('active-flag');
         $('#language-spanish').removeClass('active-flag');
+		$('#language-french').removeClass('active-flag');
 		$('#language-english').removeClass('active-flag');
 		$('#navbar-logo').attr('style', 'visibility: visible');
 	});
@@ -32,10 +36,26 @@
 		$('.br').hide();
 		$('.pt').hide();
         $('.es').show();
+		$('.fr').hide();
 		$('.en').hide();
 		$('#language-brazilian-portuguese').removeClass('active-flag');
 		$('#language-portuguese').removeClass('active-flag');
         $('#language-spanish').addClass('active-flag');
+		$('#language-french').removeClass('active-flag');
+		$('#language-english').removeClass('active-flag');
+		$('#navbar-logo').attr('style', 'visibility: visible');
+	});
+	
+	$('#language-french').click(function() {
+		$('.br').hide();
+		$('.pt').hide();
+        $('.es').hide();
+		$('.fr').show();
+		$('.en').hide();
+		$('#language-brazilian-portuguese').removeClass('active-flag');
+		$('#language-portuguese').removeClass('active-flag');
+        $('#language-spanish').removeClass('active-flag');
+		$('#language-french').addClass('active-flag');
 		$('#language-english').removeClass('active-flag');
 		$('#navbar-logo').attr('style', 'visibility: visible');
 	});
@@ -44,10 +64,12 @@
 		$('.br').hide();
 		$('.pt').hide();
         $('.es').hide();
+		$('.fr').hide();
 		$('.en').show();
 		$('#language-brazilian-portuguese').removeClass('active-flag');
 		$('#language-portuguese').removeClass('active-flag');
         $('#language-spanish').removeClass('active-flag');
+		$('#language-french').removeClass('active-flag');
 		$('#language-english').addClass('active-flag');
 		$('#navbar-logo').attr('style', 'visibility: visible');
 	});
